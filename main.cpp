d9ac31cae0af8f749659a29f3f2b089b76036683
KacperBieganek <kacbieganek@gmail.com>
2018-03-31 18:06:12 +0200

    add window colours

--- main.cpp
+++ main.cpp
@@ -25,11 +25,24 @@ int main(int argc,char** argv)
 	std::vector<Point> upperRightSnake;
 	std::vector<Point> bottomLeftSnake;
 	std::vector<Point> bottomRightSnake;
+	
 	initscr();
 	start_color();
+	init_pair(1,COLOR_WHITE,COLOR_RED);
+	init_pair(2,COLOR_WHITE,COLOR_GREEN);
+	init_pair(3,COLOR_WHITE,COLOR_BLUE);
+	init_pair(4,COLOR_WHITE,COLOR_BLACK);
 	getmaxyx(stdscr,height,width);
 	refresh();
+	
 	makeWindows(windowVector,height,width);
+	int counter=0;
+	for(auto win: windowVector)
+	{
+		wbkgd(win,COLOR_PAIR(++counter));
+		wrefresh(win);
+	}
+
 	initSnake(height/2,width/2,upperLeftSnake);
 	initSnake(height/2,width/2,upperRightSnake);
 	initSnake(height/2,width/2,bottomLeftSnake);
@@ -40,7 +53,6 @@ int main(int argc,char** argv)
 	drawSnake(windowVector[2],bottomLeftSnake);
 	drawSnake(windowVector[3],bottomRightSnake);
 	
-	//printw("height: %d  width: %d",height,width);
 	getch();
 	endwin();
 	return 0;
@@ -73,6 +85,8 @@ void initSnake(int height, int width, std::vector<Point>& points)
 
 void drawSnake(WINDOW* win, std::vector<Point>& snakePosition)
 {
+	wclear(win);
+	box(win,'*','*');
 	for(auto point : snakePosition)
 	{
 		mvwprintw(win,point.x,point.y,"o");
