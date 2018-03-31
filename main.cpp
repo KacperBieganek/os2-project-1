36059fa6d582d7ec53025aea79c7b20c8ac26450
KacperBieganek <kacbieganek@gmail.com>
2018-03-31 17:56:02 +0200

    add initSnake, drawSnake

--- main.cpp
+++ main.cpp
@@ -4,36 +4,53 @@
 #include <ncurses.h>
 
 std::mutex mx;
+bool running = true;
 
 
 struct Point{
-	int y,x;
+	int x,y;
+	Point(int x,int y): x(x),y(y) {};
 };
 
 void makeWindows(std::vector<WINDOW*>& windowVector,int height,int width);
 WINDOW* create_newwin(int height, int width, int starty, int startx);
-void initSnake(WINDOW* win,std::vector<Point>& points);
+void initSnake(int height, int width, std::vector<Point>& points);
+void drawSnake(WINDOW* win, std::vector<Point>& snakePosition);
 
 int main(int argc,char** argv)
 {
 	int width,height;
 	std::vector<WINDOW*> windowVector;
+	std::vector<Point> upperLeftSnake;
+	std::vector<Point> upperRightSnake;
+	std::vector<Point> bottomLeftSnake;
+	std::vector<Point> bottomRightSnake;
 	initscr();
 	start_color();
 	getmaxyx(stdscr,height,width);
 	refresh();
-	makeWindows(windowVector,width,height);
+	makeWindows(windowVector,height,width);
+	initSnake(height/2,width/2,upperLeftSnake);
+	initSnake(height/2,width/2,upperRightSnake);
+	initSnake(height/2,width/2,bottomLeftSnake);
+	initSnake(height/2,width/2,bottomRightSnake);
+	
+	drawSnake(windowVector[0],upperLeftSnake);
+	drawSnake(windowVector[1],upperRightSnake);
+	drawSnake(windowVector[2],bottomLeftSnake);
+	drawSnake(windowVector[3],bottomRightSnake);
+	
 	//printw("height: %d  width: %d",height,width);
 	getch();
 	endwin();
 	return 0;
 }
 
-void makeWindows(std::vector<WINDOW*>& windowVector, int width, int height)
+void makeWindows(std::vector<WINDOW*>& windowVector, int height, int width)
 {
 	windowVector.push_back(create_newwin(height/2,width/2,0,0));
-	windowVector.push_back(create_newwin(height/2,width/2,height/2,0));
 	windowVector.push_back(create_newwin(height/2,width/2,0,width/2));
+	windowVector.push_back(create_newwin(height/2,width/2,height/2,0));
 	windowVector.push_back(create_newwin(height/2,width/2,height/2,width/2));
 }
 
@@ -44,5 +61,22 @@ WINDOW *create_newwin(int height, int width, int starty, int startx)
 	box(local_win, '*' , '*');
 	wrefresh(local_win);
 	return local_win;
+}
+
+void initSnake(int height, int width, std::vector<Point>& points)
+{
+	for(int i=0;i<5;i++)
+	{
+		points.push_back(Point(height/2,width/2+i));
+	}
+}
+
+void drawSnake(WINDOW* win, std::vector<Point>& snakePosition)
+{
+	for(auto point : snakePosition)
+	{
+		mvwprintw(win,point.x,point.y,"o");
+	}
+	wrefresh(win);
 }
 
